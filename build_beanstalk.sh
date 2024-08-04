2078cf0439d15e2e5a79267fc8264bdcb268e2fa
Colin Hayes <colinmhayes@gmail.com>
2024-08-04 14:06:34 -0500

    beanstalk build script

---
+++ build_beanstalk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+destination="beanstalk/"
+zipPrefix="vacuumflask-"
+zipPostfix=$(date '+%Y%m%d')
+zipFileName="$zipPrefix$zipPostfix.zip"
+mkdir "$destination"
+cp -a templates/. "$destination/templates"
+cp -a static/. "$destination/static"
+cp app.py "$destination"
+cp Dockerfile "$destination"
+cp hash.py "$destination"
+cp objects.py "$destination"
+cp requirements.txt "$destination"
+cd "$destination"
+zip -r "../$zipFileName" "."
+cd ../
+rm -r "$destination"
